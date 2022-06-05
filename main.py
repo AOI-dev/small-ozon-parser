e56953bf85330c5c05d40203e39ffd372913a8f7
Oleg Alexandrov <32840029+AOI-dev@users.noreply.github.com>
2022-06-05 22:51:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ result = []
 html = ''
 for path in os.listdir(r"html"):
     with open(file=f"html\\{path}", encoding='utf-8', mode='r') as f:
-        if Path(path).suffix == 'html':
+        if Path(path).suffix == '.html':
             html = '\n'.join(f.readlines())
+            soup = BeautifulSoup(html, 'html.parser')
 
-soup = BeautifulSoup(html, 'html.parser')
+            # развернуть комментарии (нажать на все ссылки "Читать далее..."):
+            # document.querySelectorAll('[data-review-id] > div:nth-child(2) > div:nth-child(2) span').forEach(element => element.click())
 
-# развернуть комментарии (нажать на все ссылки "Читать далее..."):
-# document.querySelectorAll('[data-review-id] > div:nth-child(2) > div:nth-child(2) span').forEach(element => element.click())
+            for soup in soup.select('[data-review-id] > div:nth-child(2) > div:nth-child(2) span'):
+                soup: BeautifulSoup
 
-for soup in soup.select('[data-review-id] > div:nth-child(2) > div:nth-child(2) span'):
-    soup: BeautifulSoup
+                if 'Комментарий' in soup.parent.parent.get_text():
+                    string = soup.get_text().replace(' ', '')
+                    string = string.replace('\n', ' ')
 
-    if 'Комментарий' in soup.parent.parent.get_text():
-        string = soup.get_text().replace(' ', '')
-        string = string.replace('\n', ' ')
-
-        result.append(f"){string}\n")
+                    result.append(f"){string}\n")
 
 with open(file='result.txt', encoding='utf-8', mode='w') as f:
     f.writelines(result)
